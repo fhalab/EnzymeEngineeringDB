f776ef2c35faf5fa2d1d4bd0ba1feae2beaa2c03
Ariane Mora <ariane.n.mora@gmail.com>
2025-08-12 20:40:06 -0700

    Updated notebooks

--- analysis/make_structures_LLM_DS.py
+++ analysis/make_structures_LLM_DS.py
@@ -13,11 +13,23 @@ seq_col = 'aa_sequence'
 substrate_col = 'substrate'
 intermediate_col = 'intermediate'
 # LLM structures
-df = pd.read_csv('output/parents_LLM_extraction_v2.csv')
-df['intermediate'] = ['.'.join(s.split('>>')[0].split('.')[1:]) for s in df['smiles_reaction'].values]
-df['substrate'] = [s.split('>>')[0].split('.')[0] for s in df['smiles_reaction'].values]
-#df['id'] = [f'chai{i}' for i in range(0, len(df))]
-#df.to_csv('output/parents_LLM_extraction_annot.csv', index=False)
+# df = pd.read_csv('output/parents_LLM_extraction_v2.csv')
+# df['intermediate'] = ['.'.join(s.split('>>')[0].split('.')[1:]) for s in df['smiles_reaction'].values]
+# df['substrate'] = [s.split('>>')[0].split('.')[0] for s in df['smiles_reaction'].values]
+# #df['id'] = [f'chai{i}' for i in range(0, len(df))]
+# #df.to_csv('output/parents_LLM_extraction_annot.csv', index=False)
+# print(df)
+# for label, seq, substrate, cofactor in df[['id', seq_col, substrate_col, intermediate_col]].values:
+#         try:
+#                 cofactor = cofactor.split('.')
+#                 run_chai(label, seq, substrate, output_dir, cofactor)
+#         except:
+#                 print(label)
+
+# DEDB which were missed
+df = pd.read_csv('output/structures_to_do.csv')
+df['intermediate'] = ['.'.join(s.split('>>')[0].split('.')[1:]) for s in df['reaction_smiles'].values]
+df['substrate'] = [s.split('>>')[0].split('.')[0] for s in df['reaction_smiles'].values]
 print(df)
 for label, seq, substrate, cofactor in df[['id', seq_col, substrate_col, intermediate_col]].values:
         try:
@@ -25,7 +37,6 @@ for label, seq, substrate, cofactor in df[['id', seq_col, substrate_col, interme
                 run_chai(label, seq, substrate, output_dir, cofactor)
         except:
                 print(label)
-
 
 # DEDB structures
 # id_col = 'id'
