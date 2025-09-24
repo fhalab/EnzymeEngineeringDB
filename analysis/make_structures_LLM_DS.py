87ac6e9fe09c6292ffd670ddb54ea4d59780adcc
Ariane Mora <ariane.n.mora@gmail.com>
2025-09-24 14:26:16 -0700

    Updated notebooks

--- analysis/make_structures_LLM_DS.py
+++ analysis/make_structures_LLM_DS.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 os.environ['MKL_THREADING_LAYER'] = 'GNU'
 
-output_dir = 'DEDB_Chai_LLM/'
+#output_dir = 'DEDB_Chai_LLM/'
 output_dir = 'DEDB_Chai/'
 
 num_threads = 1
@@ -12,14 +12,14 @@ id_col = 'id'
 seq_col = 'parent_aa' #'aa_sequence'
 substrate_col = 'substrate'
 intermediate_col = 'intermediate'
-df = pd.read_csv('output/structures_to_do.csv')
-for label, seq, substrate, cofactor in df[['id', seq_col, substrate_col, intermediate_col]].values:
-        if isinstance(cofactor, str):
-                cofactor = cofactor.split('.')
-        else:
-                cofactor = ''
-        run_chai(label, seq, substrate, output_dir, cofactor)
-        print(label)
+# df = pd.read_csv('output/structures_to_do.csv')
+# for label, seq, substrate, cofactor in df[['id', seq_col, substrate_col, intermediate_col]].values:
+#         if isinstance(cofactor, str):
+#                 cofactor = cofactor.split('.')
+#         else:
+#                 cofactor = ''
+#         run_chai(label, seq, substrate, output_dir, cofactor)
+#         print(label)
 
 # LLM structures
 # df = pd.read_csv('output/parents_LLM_extraction_v2.csv')
@@ -35,11 +35,25 @@ for label, seq, substrate, cofactor in df[['id', seq_col, substrate_col, interme
 #         except:
 #                 print(label)
 
-# DEDB which were missed
+df = pd.read_csv('output/parents_LLM_extraction_annot.csv')
+# LLM structures that were missed, fold without the structure
+# df = pd.read_csv('output/parents_LLM_extraction_v2_missed.csv')
+df['substrate'] = ''
+print(df)
+seq_col = 'aa_sequence'
+for label, seq, substrate in df[['id', seq_col, substrate_col]].values:
+        if label == 'chai86':
+                run_chai(label, seq, substrate, output_dir)
+
+
+#DEDB which were missed
 # df = pd.read_csv('output/structures_to_do.csv')
 # df['intermediate'] = ['.'.join(s.split('>>')[0].split('.')[1:]) for s in df['reaction_smiles'].values]
 # df['substrate'] = [s.split('>>')[0].split('.')[0] for s in df['reaction_smiles'].values]
 # print(df)
+# for label, seq, substrate in df[['id', seq_col, substrate_col]].values:
+#         run_chai(label, seq, substrate, output_dir)
+
 # for label, seq, substrate, cofactor in df[['id', seq_col, substrate_col, intermediate_col]].values:
 #         try:
 #                 cofactor = cofactor.split('.')
